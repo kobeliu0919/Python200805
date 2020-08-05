699f775297400d23f9681038aab4a858471b8387
kobeliu0919 <69131746+kobeliu0919@users.noreply.github.com>
2020-08-05 11:41:06 +0800

    Add files via upload

---
+++ Day 3-2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 09:59:41 2020
+
+@author: USER
+"""
+def add3(x,y,z):
+    return x+y+z
+
+w=add3(x=int(input("number \n")),y=int(input("number \n")),z=int(input("number\n")))  
+print()
+print(w)
---
+++ Day 3-3.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 10:38:22 2020
+
+@author: USER
+"""
+hs=0
+ls=100
+def As(n):
+    ts=0
+    for i in range(n):
+       score.append(int(input("How does he score")))
+    for item in range(n):
+        ts=ts+score[item]
+    return ts/n 
+    
+name=[]
+score=[]
+a=int(input("How many people in the class"))
+print(As(a))
+def Highest_score(w):
+    if t > hs:
+        hs=t
+    else:
+        hs=hs
+    return(hs)
+for t in score:
+     Highest_score
+print(Highest_score(hs))
+def Lowest_score(v):
+    if t < ls:
+        ls=t
+    else:
+        ls=ls
+    return(ls)
+for t in score:
+     Lowest_score
+print(Lowest_score(ls))
